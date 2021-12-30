f2cfdaa3fd04ff7aa15a7e08ee4d23fa35d4a6dd
andy <ali@junglescout.com>
2021-12-30 19:06:38 +0800

    remove key

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -9,7 +9,3 @@ new CdkpipelinesDemoPipelineStack(app, "CdkpipelinesDemoPipelineStack", {
 });
 
 app.synth();
-
-// aws secretsmanager create-secret --region us-east-1 --name github-token --secret-string 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
-
-// npx cdk bootstrap --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess aws://213528851320/us-east-1
